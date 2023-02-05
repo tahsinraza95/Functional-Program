4a0bac4123957b6d421ad8f9110327803352fae0
tahsinraza95 <tahsinr693@gmail.com>
2023-02-06 04:54:06 +0530

    updated

---
+++ Even and Odd/Even and Odd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Even_and_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Enter a number : ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+                Console.Read();
+            }
+        }
+
+    }
+}
+
